f1bc8b2e56f7cf583f1bc8ae44d691c46352d29c
ducktrshessami <jwee761@gmail.com>
2019-04-15 23:10:50 -0700

    progress idk

---
+++ src/bot.js
@@ -0,0 +1,66 @@
+const EventEmitter = require('events'); // Event emitter
+const Discord = require("discord.js"); // Discord
+const colors = require("colors"); // Colored log
+
+module.exports = class bot extends EventEmitter {
+	constructor(token, t) { // Bot constructor
+		super();
+		
+		this.client = new Discord.Client();
+		this.target = t;
+		this.allies = null;
+		
+		this.client.on("ready", () => {
+			console.log("Logged in: ".green + this.client.user.username + '#' + this.client.user.discriminator);
+		});
+		
+		this.client.login(token).catch(console.log);
+	}
+	
+	ally(a) { // Identify ally bots
+		this.allies = a;
+		this.client.on("voiceStateUpdate", () => {
+			this.guildCheck(this);
+		});
+		this.guildCheck(this); // Initial check for target
+	}
+	
+	newTarget(t) { // Acquire a new target user by ID
+		this.target = t;
+		this.guildCheck(this);
+	}
+	
+	guildCheck(b) { // Check all guilds for target and correct conditions
+		if (b.target) {
+			var found = false;
+			b.client.guilds.tap((guild) => {
+				if (guild.members.has(b.target)) {
+					const t = guild.members.get(b.target);
+					if (t.voiceChannel) {
+						if (!t.voiceChannel.members.some(member => !(b.allies.includes(member.id) || member.id == b.target))) {
+							found = true;
+							b.emit("targetAcquired", t.voiceChannel);
+						}
+					}
+				}
+			});
+			if (!found) {
+				b.client.voiceConnections.tap(connection => connection.disconnect()); // Target lost
+			}
+		}
+		else {
+			b.client.voiceConnections.tap(connection => connection.disconnect()); // Target abandoned
+		}
+	}
+	
+	attack(channel) { // Actions to be taken when target is acquired
+		return new Promise((resolve, reject) => {
+			channel.join().then(resolve).catch(reject);
+		});
+	}
+	
+	disconnect() { // Disconnect from Discord
+		this.client.voiceConnections.tap(connection => connection.disconnect());
+		this.client.destroy();
+	}
+};
---
+++ src/index.js
@@ -0,0 +1,65 @@
+//const Discord = require("discord.js"); // Discord
+const colors = require("colors"); // Colored log
+const readline = require("readline"); // Console input
+const fs = require('fs'); // File output
+const bot = require("./bot"); // Discord bot class
+var config = require("../config.json"); // Bot configuration
+
+process.chdir(__dirname); // Sync working directory for fs
+var bots = config.tokens.map(token => new bot(token, config.target)); // Start all the bots
+const ios = readline.createInterface({ // Open console for commands
+	input: process.stdin,
+	output: process.stdout
+});
+
+function update() { // Update config.json
+	return new Promise((resolve, reject) => {
+		fs.writeFile("../config.json", JSON.stringify(config, null, 4), (error) => {
+			if (error) {
+				reject(error);
+			}
+			else {
+				resolve();
+			}
+		});
+	});
+}
+
+function thething(connection) {
+	
+}
+
+function init() { // Identify ally bots to each other
+	const allies = bots.map(bot => bot.client.user.id);
+	bots.forEach((bot) => {
+		bot.ally(allies);
+		bot.on("targetAcquired", (channel) => {
+			bot.attack(channel).then(thething).catch(console.log);
+		});
+	});
+	console.log("Allies identified; program initiated".green);
+}
+
+function target(t) {
+	config.target = t;
+	update().then(() => {
+		console.log("Changed target".green);
+	}).catch(console.log);
+}
+
+function exit() { // Wrap everything up
+	ios.close();
+	console.log("Disconnecting all bots from voice channels".yellow);
+	bots.forEach(bot => bot.disconnect());
+	throw "Logging off".red;
+}
+
+ios.on("line", (line) => { // Handle console commands
+	var args = line.trim().split(' '), param = args.slice(1).join(' ');
+	switch (args[0].toLowerCase()) {
+		default: console.log("Available commands: init, target, exit".white); break; // Invalid command
+		case "init": init(); break;
+		case "target": target(param); break;
+		case "exit": exit(); break;
+	}
+});
